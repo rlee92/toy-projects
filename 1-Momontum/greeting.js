00a437e8a460229f3a7ac6d1e753d4cb66f88212
Ryan Lee <ryan.hg.lee@gmail.com>
2018-11-29 23:47:17 -0800

    Create form takes username and display on the page

---
+++ 1-Momontum/greeting.js
@@ -0,0 +1,37 @@
+const form = document.querySelector(".js-form")
+const greeting = document.querySelector(".js-greetings")
+const input = form.querySelector("input")
+
+const USER_LS = "currentUser"
+const SHOWING_CN = "showing"
+
+function handleSubmit(event){
+  event.preventDefault()
+  const currentValue = input.value
+  paintGreeting(currentValue)
+}
+
+function askForName() {
+  form.classList.add(SHOWING_CN)
+  form.addEventListener("submit", handleSubmit)
+}
+
+function paintGreeting(text){
+  form.classList.remove(SHOWING_CN)
+  greeting.classList.add(SHOWING_CN)
+  greeting.innerText = `Hello ${text}`
+}
+
+function loadName(){
+  const currentUser = localStorage.getItem(USER_LS)
+  if(currentUser === null) {
+    askForName()
+  } else {
+    paintGreeting(currentUser)
+  }
+}
+
+function init() {
+  loadName()
+}
+init()
