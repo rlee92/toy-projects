0b8a72df53b1657aa7b88e57c173e40c5d4f6872
Ryan Lee <ryan.hg.lee@gmail.com>
2018-12-02 01:45:54 -0800

    Save to do items in local storage with id

--- 1-Momontum/todo.js
+++ 1-Momontum/todo.js
@@ -5,20 +5,27 @@ const toDoList = document.querySelector(".js-toDoList")
 const TODOS_LS = "toDos"
 const toDos = []
 
+function saveToDos(){
+  localStorage.setItem(TODOS_LS, JSON.stringify(toDos))
+}
+
 function paintToDo(text) {
   const li = document.createElement("li")
   const delBtn = document.createElement("button")
   const span = document.createElement("span")
+  const newId = toDos.length+1
   delBtn.innerText = "X"
   span.innerText = text
   li.appendChild(delBtn)
   li.appendChild(span)
+  li.id = newId
   toDoList.appendChild(li)
   const toDoObj = {
     text: text,
-    id: toDos.length + 1
+    id: newId
   }
   toDos.push(toDoObj)
+  saveToDos()
 }
 
 function handleSubmit(event){
