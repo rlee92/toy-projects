7e2014182e84be7028f264b55fd13dd2ea104f36
Ryan Lee <ryan.hg.lee@gmail.com>
2018-12-03 15:12:12 -0800

    Press button to delete to do items

--- 1-Momontum/todo.js
+++ 1-Momontum/todo.js
@@ -3,7 +3,19 @@ const toDoInput = toDoForm.querySelector("input")
 const toDoList = document.querySelector(".js-toDoList")
 
 const TODOS_LS = "toDos"
-const toDos = []
+let toDos = []
+
+
+function deleteToDo(event) {
+  const btn = event.target
+  const li = btn.parentNode
+  toDoList.removeChild(li)
+  const cleanToDos = toDos.filter(function(toDo){
+    return toDo.id !== parseInt(li.id)
+  })
+  toDos = cleanToDos
+  saveToDos()
+}
 
 function saveToDos(){
   localStorage.setItem(TODOS_LS, JSON.stringify(toDos))
@@ -15,6 +27,7 @@ function paintToDo(text) {
   const span = document.createElement("span")
   const newId = toDos.length+1
   delBtn.innerText = "X"
+  delBtn.addEventListener("click", deleteToDo)
   span.innerText = text
   li.appendChild(delBtn)
   li.appendChild(span)
