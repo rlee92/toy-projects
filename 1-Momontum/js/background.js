d84fece020eac98ddf32232c505d31e9121a160d
Ryan Lee <ryan.hg.lee@gmail.com>
2018-12-09 01:34:07 -0800

    background image always be 16:9 image

--- 1-Momontum/js/background.js
+++ 1-Momontum/js/background.js
@@ -3,7 +3,9 @@ const IMG_NUM = 3
 
 let getImage = (imgNum) => {
   const image = new Image()
-  image.src=`images/${imgNum+1}.jpg`
+  // image.src=`images/${imgNum+1}.jpg`
+  image.src = "https://picsum.photos/1920/1080/?random"
+  // "https://source.unsplash.com/random"
   image.classList.add("bgImage")
   body.prepend(image)
 }
