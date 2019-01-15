549840c2a6cec56a3a6be8a0da727d5158391090
Ryan Lee <ryan.hg.lee@gmail.com>
2019-01-14 21:35:15 -0800

    Created Movie DB with delete function

--- 2-CineQL/index.js
+++ 2-CineQL/index.js
@@ -1,9 +1,7 @@
 import { GraphQLServer } from "graphql-yoga"
 import resolvers from "./graphql/resolvers";
 
-const server = new GraphQLServer({
-  typeDefs: "graphql/schema.graphql",
-  resolvers
-});
+const typeDefs = "graphql/schema.graphql"
+const server = new GraphQLServer({typeDefs, resolvers})
 
 server.start(() => console.log('Server is running on localhost:4000'))
