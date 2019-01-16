8912de9e8850c987315b80dc6cc89836bf8a66e4
Ryan Lee <ryan.hg.lee@gmail.com>
2019-01-15 16:27:30 -0800

    Add + Delete Movie

--- 2-CineQL/graphql/db.js
+++ 2-CineQL/graphql/db.js
@@ -29,11 +29,21 @@ export const getById = id => {
 }
 
 export const deleteMovie = id => {
-  const cleanedMovies = movies.filter(movie => movie.id !== String(id))
+  const cleanedMovies = movies.filter(movie => movie.id !== id)
   if (movies.length > cleanedMovies.length) {
     movies = cleanedMovies
     return true;
   } else {
     return false;
   }
+}
+
+export const addMovie = (name, score) => {
+  const newMovie = {
+    id: `${movies.legnth + 1}`,
+    name,
+    score
+  }
+  movies.push(newMovie)
+  return newMovie
 }
