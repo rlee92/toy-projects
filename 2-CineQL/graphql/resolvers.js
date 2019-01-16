1f3a76b948b2672ae2457d14b59e593079761685
Ryan Lee <ryan.hg.lee@gmail.com>
2019-01-15 22:59:53 -0800

    axios init

--- 2-CineQL/graphql/resolvers.js
+++ 2-CineQL/graphql/resolvers.js
@@ -1,9 +1,10 @@
-import { getMovies } from "./db";
+import { getMovies, getMovie } from "./db"
 
 const resolvers = {
   Query: {
-    movies: (_, { rating, limit }) => getMovies(limit, rating)
+    movies: (_, { rating, limit }) => getMovies(limit, rating),
+    movie: (_, { id }) => getMovie(id)
   }
 }
 
-export default resolvers;
+export default resolvers
