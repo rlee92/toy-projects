2d44a25ea7cdd6144b243a7c21eac1f0ead01b22
rlee92 <ryan.hg.lee@gmail.com>
2019-02-05 16:57:01 -0800

    Minor syntax change

--- 2-CineQL/graphql/db.js
+++ 2-CineQL/graphql/db.js
@@ -1,9 +1,9 @@
 import axios from "axios"
-const LIST_MOVIES_URL = "https://yts.am/api/v2/list_movies.json?"
+const MOVIES_LIST_URL = "https://yts.am/api/v2/list_movies.json?"
 const MOVIE_DETAILS_URL = "https://yts.am/api/v2/movie_details.json"
 
 export const getMovies = async (limit, rating) => {
-  let REQUEST_URL = LIST_MOVIES_URL
+  let REQUEST_URL = MOVIES_LIST_URL
   if (limit > 0) {
     REQUEST_URL += `limit=${limit}`
   }
--- 2-CineQL/index.js
+++ 2-CineQL/index.js
@@ -1,7 +1,7 @@
 import { GraphQLServer } from "graphql-yoga"
-import resolvers from "./graphql/resolvers";
+import resolvers from "./graphql/resolvers"
 
 const typeDefs = "graphql/schema.graphql"
-const server = new GraphQLServer({typeDefs, resolvers})
+const server = new GraphQLServer({ typeDefs, resolvers })
 
-server.start(() => console.log('Server is running on localhost:4000'))
+server.start(_ => console.log('Server is running on localhost:4000'))
